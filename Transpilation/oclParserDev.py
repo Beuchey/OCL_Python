9a2a08a5692e94b87818ab1c8f6da8d938c3b114
Kero <grrrrrrgrrrrrrrrrrrr@gmail.com>
2019-04-06 00:07:55 +0200

    _

--- Transpilation/oclParserDev.py
+++ Transpilation/oclParserDev.py
@@ -12,8 +12,6 @@ from textx.model import get_metamodel
 
 
 
-
-
 # Output tools
 
 def filter(expression):
@@ -23,7 +21,7 @@ def filter(expression):
         if e is not None and e[0]!='_' and e!='parent':
             content = elements[e]
             contentType = type(content)
-            if(contentType==list or contentType==str):
+            if(contentType==list or contentType==str or contentType==int):
                 result[e] = content
             else:
                 result[e] = content.__class__.__name__
@@ -60,23 +58,9 @@ def introduce(expression, expressionDescription, level):
     log(level, "FOUND : ", expressionDescription, " : \n", tabulate(level+1), filter(expression), "\n")
 
 def delegate(elements, identifier, level):
-    """def delegate(elements, identifier, level, useSymbols=False, leftSymbol="[", rightSymbol="]", separatorSymbol=","):"""
     log(level+1, "***", identifier, " : ")
     content = elements[identifier]
     if(type(content)==list):
-        """
-        result = ""
-        if useSymbols:
-            result += leftSymbol
-        result += defaultExpressionParser(content[0], level+1)
-        for e in content[1:]:
-            if useSymbols:
-                result += separatorSymbol
-            result += " " + defaultExpressionParser(e, level+1)
-        if useSymbols:
-            result += rightSymbol
-        return result
-        """
         return defaultExpressionParser(content[0], level+1)
     else:
         return defaultExpressionParser(content, level+1)
@@ -88,7 +72,12 @@ def splitInfix(elements, operatorName, leftExpressionName, rightExpressionName, 
     else:
         return delegate(elements, leftExpressionName, level)
 
-
+def extract(expression):
+    elements = vars(expression)
+    for e in elements:
+        current = elements[e]
+        if current is not None:
+            return current
 
 
 
@@ -114,6 +103,11 @@ def extractAtribute(e):
 def defaultExpressionParser(expression, level):
     log(level, "!!!DEFAULT!!!\n", tabulate(level+1), expression, "\n")
     return "!!!DEFAULT!!!"
+
+@defaultExpressionParser.register(metamodel["Expression"])
+def expressionParser(expression, level):
+    introduce(expression, "Expression", level)
+    return defaultExpressionParser(extract(expression), level+1)
 
 @defaultExpressionParser.register(metamodel["IfExpression"])
 def ifExpressionParser(expression, level):
@@ -214,8 +208,59 @@ def actualParameterListParser(expression, level):
         result += ", " + defaultExpressionParser(e, level+1)
     return result
 
+@defaultExpressionParser.register(metamodel["PrimaryExpression"])
+def primaryExpressionParser(expression, level):
+    introduce(expression, "PrimaryExpression", level)
+    elements = vars(expression)
+    nestedExpression = elements["expression"]
+    if(nestedExpression is not None):
+        return "(" + defaultExpressionParser(nestedExpression, level+1) + ")"
+    else:
+        return defaultExpressionParser(extract(expression), level+1)
 
+@defaultExpressionParser.register(metamodel["LiteralCollection"])
+def literalCollectionParser(expression, level):
+    introduce(expression, "LiteralCollection", level)
+    elements = vars(expression)
+    result = "oclWrapper_Creator(["
+    collectionItems = elements["collectionItems"]
+    result += defaultExpressionParser(collectionItems[0], level+1)
+    for e in collectionItems[1:]:
+        result += ", " + defaultExpressionParser(e, level+1)
+    result += "])"
+    return result
 
+@defaultExpressionParser.register(metamodel["CollectionItem"])
+def collectionItemParser(expression, level):
+    introduce(expression, "CollectionItem", level)
+    elements = vars(expression)
+    result = delegate(elements, "startExpression", level)
+    endExpression = elements["endExpression"]
+    if(endExpression is not None):
+        result += ".." + defaultExpressionParser(endExpression, level+1)
+    return result
+
+@defaultExpressionParser.register(metamodel["Literal"])
+def literalParser(expression, level):
+    introduce(expression, "Literal", level)
+    return defaultExpressionParser(extract(expression), level+1)
+
+@defaultExpressionParser.register(int)
+def numberParser(expression, level):
+    return str(expression)
+
+@defaultExpressionParser.register(str)
+def stringParser(expression, level):
+    return str(expression)
+
+@defaultExpressionParser.register(metamodel["EnumLiteral"])
+def enumLiteralParser(expression, level):
+    introduce(expression, "EnumLiteral", level)
+    names = vars(expression)["names"]
+    result = defaultExpressionParser(names[0], level+1) + "::" + defaultExpressionParser(names[1], level+1)
+    for e in names[2:]:
+        result += ", " + defaultExpressionParser(e, level+1)
+    return result
 
 
 
@@ -223,6 +268,8 @@ def actualParameterListParser(expression, level):
 # WHERE THE MAGIC HAPPENS
 
 model = metamodel.model_from_file("expression.ocl")
+
+res(0, "import sys, os\nsys.path.insert(0, os.path.join(os.path.dirname(__file__), '../Wrapper/', 'oclpyth'))\nfrom OclPyth import oclWrapper_Creator\n\n")
 
 for expression in model.expressions:
     res(0, defaultExpressionParser(expression, 0), "\n")
